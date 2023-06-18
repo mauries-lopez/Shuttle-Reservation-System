76b140bec3d596d4d5ba879f8aa21884a78ce93b
Mauries Lopez <mauilopez00@gmail.com>
2023-06-18 16:17:20 +0800

    Added Reservation feature
    - Changed reservation ui
    - Added javascript for reserving schedule

--- [MCO] Phase-1/JS/Reservation.js
+++ [MCO] Phase-1/JS/Reservation.js
@@ -6,4 +6,19 @@ function rightClick(){
     btn.style.left = '160px';
 }
 
+function showScheduleForm() {
+    var scheduleForm = document.getElementsByClassName('form_box')[0];
+    scheduleForm.style.display = 'block';
+}
 
+function hideScheduleForm(){
+    var scheduleContainer = document.getElementsByClassName('schedule_container')[0];
+    var scheduleForm = document.getElementsByClassName('form_box')[0];
+
+    var div = document.createElement('div');
+    div.className = 'reserved_schedule';
+
+    scheduleContainer.appendChild(div);
+    
+    scheduleForm.style.display = 'none';
+}
